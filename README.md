e64d929d66302d1483e4fe1a97da8ac5099b757c
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-09-18 12:56:30 +0000

    pakeista tema

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # fp-2024
-## Domain - Public transport
+## Domain - Public transport and routes
 **Main entities:**
-Busses and trains
+- Busses
+- Trains
+- Bus routes
+- Train routes
+- Public paths
+
 
 **Main operations:**
+- Add/Remove bus
+- Add/Remove train
+- Add/Remove bus/train stop
+- Create a route
+- Assign bus/train to routes
+- Remove bus/train from routes
+- Add bus/train stop to route
+- Create a public path (a path from one stop to other stop)
+- Remove public path
 
+- Print all the routes
+- Print all the busses/trains
+- Print all the route stops
+- Print all the route busses/trains
+- Join two routes at the intersecting stop (creates a new route)
+- Join two routes at the provided stop (creates a new route)
 
-Examples: to do
+Yes the same route will be able to have both busses and trains operating.
--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,12 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = ["add_book remove_book list_books create_collection add_to_collection remove_from_collection list_collection merge_collections remove_collection"]
+completions = [
+    "add_bus ", "remove_bus ", "add_train ", "remove_train ", "add_bus_stop ", 
+    "remove_bus_stop ", "add_train_stop ", "remove_train_stop ", "create_route ", 
+    "assign_bus_to_route ", "assign_train_to_route ", "remove_bus_from_route ", 
+    "remove_train_from_route ", "add_bus_stop_to_route ", "remove_bus_stop_from_route ", 
+    "add_train_stop_to_route ", "remove_train_stop_from_route ", "print_all_routes ", 
+    "print_all_busses ", "print_all_trains ", "print_all_route_busses ", 
+    "print_all_route_trains ", "join_two_routes ", "join_two_routes_at_stop ", 
+    "create_path ", "remove_path "]
